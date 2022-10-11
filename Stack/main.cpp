1333078274b5e1c1d6310a164aa9f8df76c360b6
mitchcamza <tide.mute_06@icloud.com>
2022-10-11 05:58:34 +0200

    Initial Commit

---
+++ Stack/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Stack
+//
+//  Created by Mitch Campbell on 2022/10/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
